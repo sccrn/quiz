1ad156a50e7cd72ef0ae6f97bd9c763bf45a3cae
Samanta Coutinho <sccrn@hotmail.com>
2019-08-15 23:25:01 -0400

    Project's creation.

---
+++ quiz/quiz/Core/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  quiz
+//
+//  Created by Samanta Coutinho on 2019-08-15.
+//  Copyright © 2019 Samanta Coutinho. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ quiz/quiz/Scenes/App/BaseController.swift
@@ -0,0 +1,9 @@
+//
+//  BaseController.swift
+//  quiz
+//
+//  Created by Samanta Coutinho on 2019-08-15.
+//  Copyright © 2019 Samanta Coutinho. All rights reserved.
+//
+
+import Foundation
